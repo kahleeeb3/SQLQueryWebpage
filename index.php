6206250c6baaf4bf60753af64886393a68b207e9
Caleb Powell <powellcalebm@gmail.com>
2023-04-11 00:51:50 -0500

    remove uneeded files

--- index.php
+++ index.php
@@ -7,9 +7,9 @@
 
     <!-- Code Syntax Highlighting -->
     <link rel="stylesheet" href="./src/css/prism.css" />
-    <link rel="stylesheet" href="./src/css/prism-live.css" />
     <script src="./src/js/prism.js"></script>
-    <script src="./src/js/prism-live.js"></script>
+
+
 </head>
 
 <body class="background">
@@ -24,27 +24,17 @@
             <h2>Database: cmp0132</h2>
         </div>
         <div class="right">
-            <h2>Queries Input</h2>
-            <form>
-                <label for="sql-query">Enter your SQL query:</label>
-                <br>
-                <textarea id="sql-query" name="sql-query"></textarea>
-                <br>
-                <input type="submit" value="Run Query">
-            </form>
 
-            <script>
-                Prism.highlightAll();
-                Prism.languages.sql = Prism.languages.extend('sql', {});
-            </script>
+            <div>
+                <pre>
+                    <code class="language-sql" contenteditable="true" id="editable-code">
+                    SELECT * FROM Customer
+                    </code>
+                </pre>
+            </div>
 
 
-            <!-- Import Prims.js and initialize syntax highlighting -->
 
-
-            <!-- <script type="text/plain" class="language-sql">
-                SELECT
-            </script> -->
         </div>
     </div>
 
@@ -52,7 +42,3 @@
 </body>
 
 </html>
-
-<script>
-    Prism.highlightAll();
-</script>
